36e65cdd52e11b264e786368daf08f60e980132a
Josh Singer <josh.singer@glasgow.ac.uk>
2019-08-05 11:02:04 +0100

    Fixed various issues which meant that the project was not building.

--- glue/loadExogenousData.js
+++ glue/loadExogenousData.js
@@ -1,4 +1,4 @@
-// list the sequences in source ncbi-refseqs
+// list the sequences in source ncbi-curated
 var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-curated'"]);
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
@@ -7,7 +7,7 @@ _.each(seqIds, function(seqId) {
     // create an object in the custom table which uses the sequence ID as the row ID.
     glue.command(["create", "custom-table-row", "isolate_data", seqId]);
     // associate the corresponding sequence with this object.
-    glue.inMode("sequence/ncbi-refseqs/"+seqId, function() {
+    glue.inMode("sequence/ncbi-curated/"+seqId, function() {
         glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
     });
 });
